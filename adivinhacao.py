0f9c1f1432698f9dd7f38b5cc2161873a45ba8b7
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-19 11:06:56 -0300

    FEAT02 - ADICIONANDO COMPARACAO DE VARIAVEIS

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,1 +1,14 @@
+print("*********************************")
 print("Bem vindo ao jogo de adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute = input("Digite o seu numero: ")
+
+print("Você digitou", chute)
+
+if(numero_secreto == chute):
+    print("Você Acertou!")
+else:
+    print("Você Errou!")  
