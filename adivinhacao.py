2bc2240b3bdaaa48680023fe9d12f8e0ee401561
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-19 19:59:45 -0300

    ADICIONANDO LIMITACOES NO CHUTE E OS COMANDOS CHAVES BREAK E CONTINUE NO LOOP

--- adivinhacao.py
+++ adivinhacao.py
@@ -7,21 +7,27 @@ total_de_tentativas = 3
 rodada = 1
 
 for rodada in range(1, total_de_tentativas + 1):
-  print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-  chute = int(input("Digite o seu numero: "))
-  print("Você digitou", chute)
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = int(input("Digite um número entre 1 e 100: "))
+    print("Você digitou", chute)
 
-  acertou = chute == numero_secreto
-  maior   = chute > numero_secreto
-  menor   = chute < numero_secreto
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
-  if(acertou):
-      print("Você Acertou!")
-  else:
-      if(maior):
-          print("Você Errou! O seu chute foi maior do que o número secreto.")
-      elif(menor):
-          print("Você Errou! O seu chute foi menor do que o número secreto.")
+
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
+
+    if(acertou):
+        print("Você Acertou!")
+        break
+    else:
+        if(maior):
+            print("Você Errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você Errou! O seu chute foi menor do que o número secreto.")
 
 
 print("Fim do Jogo!")
