56c79ac67bd39f15eeb4c638436384f3a7904078
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-19 17:48:51 -0300

    ADICIONANDO LOOP PARA TENTATIVAS DE ACERTOS E ERROS

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,20 +3,27 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite o seu numero: "))
+while(rodada <= total_de_tentativas):
+  print("Tentativa", rodada, "de", total_de_tentativas)
+  chute = int(input("Digite o seu numero: "))
+  print("Você digitou", chute)
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+  acertou = chute == numero_secreto
+  maior   = chute > numero_secreto
+  menor   = chute < numero_secreto
 
-print("Você digitou", chute)
+  if(acertou):
+      print("Você Acertou!")
+  else:
+      if(maior):
+          print("Você Errou! O seu chute foi maior do que o número secreto.")
+      elif(menor):
+          print("Você Errou! O seu chute foi menor do que o número secreto.")
 
-if(acertou):
-    print("Você Acertou!")
-else:
-    if(maior):
-        print("Você Errou! O seu chute foi maior do que o número secreto.")
-    elif(menor):
-        print("Você Errou! O seu chute foi menor do que o número secreto.")
+      rodada = rodada + 1
+
+
 print("Fim do Jogo!")
