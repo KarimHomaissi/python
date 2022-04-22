20e0c2e15a0c09eacec20c251b5a4ed297bc6f3f
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-22 14:49:13 -0300

    ADICIONANDO ATRIBUTOS DA CLASSE CONTA

--- poo/conta.py
+++ poo/conta.py
@@ -1,3 +1,13 @@
 
 class Conta:
-    pass
+    
+    def __init__(self, numero, titular, saldo, limite):
+        print("Construindo o Objeto ... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+
+conta = Conta(1,"Karim Homaissi",100.0,1000.0)
+conta2 = Conta(2,"Marco Antonio",55.0,1000.0)
