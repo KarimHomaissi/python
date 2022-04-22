912f210ef86e01d78c36b0887ccb12cf6c9de832
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-22 14:25:40 -0300

    ADICIONANDO MAIS FUNCOES NO PARADIGMA PROCEDURAL COM RELACAO AO SISTEMA DE CONTAS

--- poo/teste.py
+++ poo/teste.py
@@ -3,5 +3,18 @@ def criar_conta(numero, titular, saldo, limite):
     conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
     return conta
 
+def depositar(conta, valor):
+    conta["saldo"] += valor
+
+def sacar(conta, valor):
+    conta["saldo"] -= valor
+
+def extrato(conta):
+    print("Saldo: {}".format(conta["saldo"]))
+
 conta1 = criar_conta(1,"Karim",100.00,1000.00)
-print(conta1["numero"],conta1["titular"])
+print(conta1["numero"],conta1["titular"],conta1["saldo"],conta1["limite"])
+sacar(conta1,50)
+extrato(conta1)
+depositar(conta1,20)
+extrato(conta1)
