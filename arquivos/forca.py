def1775fe21a13f5a7076718820a3189695501bb
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-22 10:29:41 -0300

    CRIANDO FUNCOES PARA ORGANIZACAO DO CODIGO

--- arquivos/forca.py
+++ arquivos/forca.py
@@ -1,22 +1,10 @@
 import random
 
 def jogar():
-    print("*********************************")
-    print("***Bem vindo ao jogo de Forca!***")
-    print("*********************************")
 
-
-    with open("palavras.txt","r") as arquivo:
-        palavras = []
-
-        for linha in arquivo:
-            linha = linha.strip()
-            palavras.append(linha)
-
-    numero = random.randrange(0,len(palavras))
-    palavra_secreta = palavras[numero].upper()
-
-    letras_acertadas = ["_" for letra in palavra_secreta]
+    imprime_mensagem_abertura()
+    palavra_secreta = carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
 
     enforcou = False
     acertou = False
@@ -50,6 +38,26 @@ def jogar():
         print("A Resposta era: {}".format(palavra_secreta))
 
     print("Fim do Jogo!")
+
+def imprime_mensagem_abertura():
+    print("*********************************")
+    print("***Bem vindo ao jogo de Forca!***")
+    print("*********************************")
+
+def carrega_palavra_secreta():
+    with open("palavras.txt","r") as arquivo:
+        palavras = []
+
+        for linha in arquivo:
+            linha = linha.strip()
+            palavras.append(linha)
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
 
 if(__name__ == "__main__"):
     jogar()
