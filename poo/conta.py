1ab77a6b5302d42baff03df124e156762e40950b
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-25 10:52:33 -0300

    ADICIONANDO OS METODOS SACAR, DEPOSITAR E EXTRATO NA CLASSE CONTA

--- poo/conta.py
+++ poo/conta.py
@@ -8,8 +8,22 @@ class Conta:
         self.saldo = saldo
         self.limite = limite
 
+    def extrato(self):
+        print("Saldo: R$ {}".format(self.saldo))
+
+    def depositar(self,valor):
+        self.saldo += valor
+    
+    def sacar(self,valor):
+        self.saldo -= valor
+
 
 conta = Conta(1,"Karim Homaissi",100.0,1000.0)
 conta2 = Conta(2,"Marco Antonio",55.0,1000.0)
 
 print(conta.titular,conta2.titular)
+conta.extrato()
+conta.depositar(100)
+conta.extrato()
+conta.sacar(10)
+conta.extrato()
