e03fa5b6877f030f4bd60c4f922635cc55712750
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-25 15:00:38 -0300

    ADICIONANDO METODO TRANSFERIR UTILIZANDO REFERENCIA POR PARAMETRO

--- poo/conta.py
+++ poo/conta.py
@@ -16,14 +16,21 @@ class Conta:
     
     def sacar(self,valor):
         self.__saldo -= valor
+    
+    def transferir(self,valor,destino):
+        self.sacar(valor)
+        destino.depositar(valor)
 
 
 conta = Conta(1,"Karim Homaissi",100.0,1000.0)
 conta2 = Conta(2,"Marco Antonio",55.0,1000.0)
 
-print(conta.titular,conta2.titular)
 conta.extrato()
 conta.depositar(100)
 conta.extrato()
 conta.sacar(10)
 conta.extrato()
+
+conta.transferir(90,conta2)
+conta.extrato()
+conta2.extrato()
