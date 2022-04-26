bbeac4e3ae496027153a95d331a31f9532aaf2ce
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-26 12:08:20 -0300

    APRENDENDO COMANDOS DE CONVERSAO NO CURSO BASICO DE SHELL SCRIPT

---
+++ shell_script/conversao-jpg-png.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+convert ~/Downloads/imagens-livros/asp_net.jpg ~/Downloads/imagens-livros/asp_net.png
+
+: '
+  ~ = diretorio home
+  convert = é usado através da biblioteca imagemagick para converter extensoes de imagens para outras extensoes
+  #!/bin/bash = é o interpretador que queremos utilizar ao rodar nosso shell script
+  rodamos com o comando bash conversao-jpg-png.sh
+'
+
