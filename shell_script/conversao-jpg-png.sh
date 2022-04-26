8e23b053b6ddbca8cbbb5c9d9adff69d4f8092a1
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-26 14:29:08 -0300

    ADICIONANDO MAIS COMENTARIOS PARA FACILITAR A COMPREENSAO DO SCRIPT

--- shell_script/conversao-jpg-png.sh
+++ shell_script/conversao-jpg-png.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-cd ~/Downloads/imagens-livros
+  cd ~/Downloads/imagens-livros
 
-for imagem in *.jpg
-do
-    imagem_sem_extensao = $(ls $imagem | awk -F . '{ print $1 }')
-    convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
-done
+  if [ ! -d png ] 
+  then
+      mkdir png
+  fi
+
+  for imagem in *.jpg
+  do
+      imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+      convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
+  done
 
 : '
   ~ = diretorio home
   convert = é usado através da biblioteca imagemagick para converter extensoes de imagens para outras extensoes
   #!/bin/bash = é o interpretador que queremos utilizar ao rodar nosso shell script
   rodamos com o comando bash conversao-jpg-png.sh
+  SCRIPT SHELL NAO PERMITE A SEPARACAO DO = DA VARIAVEL E DO CONTEUDO
+  DENTRO DO COLCHETES DO IF TEM QUE TER ESPACO
+  VERIFICAMOS SE EXISTE O DIRETORIO /PNG PARA PODER JOGAR AS IMAGENS CONVERTIDAS LA
 '
 
