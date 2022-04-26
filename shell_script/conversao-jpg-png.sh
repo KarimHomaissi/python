b2b10a657f6d021bb1ffcc8deced63101bf2cd32
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-26 12:18:12 -0300

    ADICIONANDO 2 PARAMETROS

--- shell_script/conversao-jpg-png.sh
+++ shell_script/conversao-jpg-png.sh
@@ -2,6 +2,7 @@
 
 CAMINHO_IMAGENS = ~/Downloads/imagens-livros
 convert $CAMINHO_IMAGENS/$1.jpg $CAMINHO_IMAGENS/$1.png
+convert $CAMINHO_IMAGENS/$2.jpg $CAMINHO_IMAGENS/$2.png
 
 : '
   ~ = diretorio home
