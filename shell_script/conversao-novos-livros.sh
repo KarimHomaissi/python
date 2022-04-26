c44450866aa212359519c3d564174950de7f113b
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-26 15:30:19 -0300

    CRIANDO NOVO ARQUIVO QUE CONVERTE RECURSIVAMENTE ENTRE DIRETORIOS IMAGENS JPG PARA PNG

---
+++ shell_script/conversao-novos-livros.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/Downloads/imagens-novos-livros
+for arquivo in *
+do
+    if [ -d $arquivo ]
+    then
+        #Entrar no diretório e fazer varredura do conteudo
+    else
+        #conversao jpg para png
+    fi
+done
