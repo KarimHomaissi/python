1406cfb9859dee07c8e65574f94202757c449353
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-26 12:22:26 -0300

    ADICIONANDO LOOP DE REPETICAO FOR E USANDO O  QUE REPASSA TODOS OS PARAMETROS POSSIVEIS COMO SE FOSSE UM RANGE

--- shell_script/conversao-jpg-png.sh
+++ shell_script/conversao-jpg-png.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 CAMINHO_IMAGENS = ~/Downloads/imagens-livros
-convert $CAMINHO_IMAGENS/$1.jpg $CAMINHO_IMAGENS/$1.png
-convert $CAMINHO_IMAGENS/$2.jpg $CAMINHO_IMAGENS/$2.png
+
+for imagem in $@
+do
+    convert $CAMINHO_IMAGENS/$imagem.jpg $CAMINHO_IMAGENS/$imagem.png
+done
 
 : '
   ~ = diretorio home
