c0525ea5c7bc25f18d0089fda3654efa01d59a2f
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-26 14:37:50 -0300

    CRIANDO UMA FUNCAO PARA ENGLOBAR TUDO E EXECUTANDO ELA

--- shell_script/conversao-jpg-png.sh
+++ shell_script/conversao-jpg-png.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-  cd ~/Downloads/imagens-livros
+converte_imagem() {
+    cd ~/Downloads/imagens-livros
+    if [ ! -d png ] 
+    then
+        mkdir png
+    fi
 
-  if [ ! -d png ] 
-  then
-      mkdir png
-  fi
+    for imagem in *.jpg
+    do
+        imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+        convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
+    done
+}
 
-  for imagem in *.jpg
-  do
-      imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
-      convert $imagem_sem_extensao.jpg png/$imagem_sem_extensao.png
-  done
+converte_imagem
+
 
 : '
   ~ = diretorio home
