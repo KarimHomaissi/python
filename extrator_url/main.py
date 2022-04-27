47f211e27e80106730d9c1ef1423196a472012da
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-27 16:30:13 -0300

    ADICIONANDO O METODO LEN E PASSANDO TEXTO PARA O METODO FIND

--- extrator_url/main.py
+++ extrator_url/main.py
@@ -1,8 +1,18 @@
 
 url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
 print(url)
+
 indice_interrogacao = url.find("?")
 url_base = url[:indice_interrogacao]
 print(url_base)
+
 url_parametros = url[indice_interrogacao + 1:]
 print(url_parametros)
+
+parametro_busca = "quantidade"
+indice_parametro = url_parametros.find(parametro_busca)
+
+
+indice_valor = indice_parametro + len(parametro_busca) + 1
+valor = url_parametros[indice_valor:]
+print(valor)
