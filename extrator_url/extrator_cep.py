28916183cb2e8fe1a4b5cd798c95066db06f2e5b
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-28 08:51:42 -0300

    SIMPLIFICANDO OS CONJUNTOS DE PADRAO ADICIONANDO CHAVES PARA DIZER QUANTAS VEZES APARECE OS DIGITOS

--- extrator_url/extrator_cep.py
+++ extrator_url/extrator_cep.py
@@ -3,7 +3,7 @@ endereco = "Rua das Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ
 import re # Regular Expression -- RegEX
 
 # 5 dígitos + hífen (opcional) + 3 dígitos
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao = re.compile("[0123456789]{5}[-]?[0123456789]{3}")
 busca = padrao.search(endereco) # return Math Object or None
 
 if busca:
