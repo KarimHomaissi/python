c5278d4f64debbb87395faef8b8232b0c18991d9
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-28 08:54:34 -0300

    MODIFICANDO A REGEX PARA DIZER DE OUTRA FORMA QUE O HIFEN PODE APARECER OU 0 VEZES OU 1 VEZ

--- extrator_url/extrator_cep.py
+++ extrator_url/extrator_cep.py
@@ -3,7 +3,7 @@ endereco = "Rua das Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ
 import re # Regular Expression -- RegEX
 
 # 5 dígitos + hífen (opcional) + 3 dígitos
-padrao = re.compile("[0-9]{5}[-]?[0-9]{3}")
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
 busca = padrao.search(endereco) # return Math Object or None
 
 if busca:
