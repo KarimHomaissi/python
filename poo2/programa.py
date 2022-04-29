5dbf2256910c7247c0f59d2e11d18834ce1a1ac1
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-29 13:32:43 -0300

    CRIANDO A CLASSE PLAYLIST

--- poo2/programa.py
+++ poo2/programa.py
@@ -38,6 +38,15 @@ class Serie(Programa):
     def __str__(self):
         return f'Nome: {self.nome} - {self.temporadas} temporadas - Likes: {self.likes}'
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+
+    def tamanho(self):
+        return len(self.programas)
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
 vingadores.dar_likes()
