7c51a803c178af24256259ccbb4117921d93e616
Karim Homaissi <karimhomaissi@gmail.com>
2022-04-29 13:33:33 -0300

    ADICIONANDO MAIS FILMES E SERIES E CRIANDO UM OBJETO DO TIPO PLAYLIST

--- poo2/programa.py
+++ poo2/programa.py
@@ -46,17 +46,24 @@ class Playlist:
     def tamanho(self):
         return len(self.programas)
 
-
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
-vingadores.dar_likes()
-vingadores.dar_likes()
-vingadores.dar_likes()
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
 
-atlanta.dar_likes()
-atlanta.dar_likes()
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
 
-listinha = [atlanta, vingadores]
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in listinha:
+for programa in filmes_e_series:
     print(programa)
